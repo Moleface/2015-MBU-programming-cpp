2744ddf2ceb31648e92e1c7f4531b7f4fa8b9209
moleface <w.haney@gmail.com>
2015-04-08 00:16:58 +0000

    Automatic commit

--- random.cpp
+++ random.cpp
@@ -9,13 +9,34 @@ int main(void){
 	int random_number = 1 + (rand() % 10); // random number between 1 and 10
 	// declare variable to store guess of user and set to 0
 	// declare variable as counter for number of guesses
-
+int MolecubeII = 0;
+int MolecubeIII = 0;
 	
 	for(;;)
 	{
 		// increment guess counter
+		MolecubeIII = MolecubeIII + 1;
+		//cout << MolecubeIII << endl;
+		//
+		
 
-		// read input from user
+		cin >> MolecubeII;
+
+		if (MolecubeII > random_number)
+		{
+		cout << "The number is too high\n";
+			
+		}else if (MolecubeII < random_number)
+		{
+		cout << "The number is too low\n";
+		
+		}else
+		{
+		cout << "The number is right\n";
+		break;
+		}
+
+		// read input from user using magic...
 		//
 		
 		// compare input from user to random number
